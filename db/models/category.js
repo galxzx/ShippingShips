662044c02aca9c599abd41ddfc3dfbc0c1f89129
Andrew Basore <basor1aj@cmich.edu>
2017-02-27 16:07:13 -0500

    Created category model

---
+++ db/models/category.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Category = db.define('category', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	}
+
+})
+
+module.exports = Category;
