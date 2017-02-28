5a10fa87dfabe9b391d61169cf19e2e9d92586bf
Andrew Basore <basor1aj@cmich.edu>
2017-02-28 16:14:46 -0500

    some admin routes for manipulating db

---
+++ server/admin/products.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+const Product = require('APP/db/models/product')
+
+router.param('productId', (req, res, next, id) => {
+	Product.findById(id)
+	.then( product => {
+		if (!product) throw new Error('Product not found');
+		req.product = product;
+		next();
+	})
+	.catch(err => {console.error(err)})
+})
+
+//Product creation route
+router.post('/', (req, res, next) => {
+
+	Product.create(req.body)
+	.then(product => res.json(product))
+	.catch(err => console.error(err));
+})
+
+//Product update route
+route.put('/:productId' (req, res, next) => {
+	req.product.update(req.body)
+	.then( product => {
+		res.json(product);
+	})
+})
+
+//Product deletion route
+router.delete('/:productId', (req, res, next) => {
+	req.product.destroy()
+	.then( res.sendStatus(204));
+})
+
+module.exports = router;
--- server/api.js
+++ server/api.js
@@ -4,12 +4,13 @@ const db = require('APP/db')
 const api = module.exports = require('express').Router()
 
 api
-  .get('/heartbeat', (req, res) => res.send({ok: true,}))
+  .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./auth'))
+  .use('/admin', require('./admin'))
   .use('/users', require('./users'))
   .use('/products', require('./products'))
   .use('/orders', require('./orders'))
-  .use('/admin', require('./admin'))
+
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
