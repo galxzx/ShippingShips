b7a0ad590f3204bef30962fce6d3021458406d66
Andrew Basore <basor1aj@cmich.edu>
2017-02-28 18:05:49 -0500

    put some JSX into Cart.JSX and made stateful container

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
-export default function Cart (){
+export default function Cart ({cart, total}){
 
 
 	return (
 	        <div>
+	        	<ul>
+	        		{
+	        			cart&&cart.map( item => {
+	        				<li key = {item.id}>
+	        					<h3>{item.title}</h3>
+					            <p>{item.description}</p>
+					            <p>Price: ${item.price}</p>
+	        				</li>
+	        			})
+	        		}
+	        	</ul>
 
 	        </div>
 	        );
