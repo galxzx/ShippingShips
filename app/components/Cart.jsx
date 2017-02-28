060243734cdf326933411335323e86eee28b5500
Andrew Basore <basor1aj@cmich.edu>
2017-02-28 18:15:47 -0500

    Added total display to Cart component

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 
-export default function Cart ({cart, total}){
-
+export default function Cart (props){
+	const cart = props.cart;
+	const total = props.total || 0;
 
 	return (
-	        <div>
-	        	<ol>
-	        		{
-	        			cart&&cart.map( item => {
-	        				<li key = {item.id}>
-	        					<h3>{item.title}</h3>
-					            <p>{item.description}</p>
-					            <p>Price: ${item.price}</p>
-	        				</li>
-	        			})
-	        		}
-	        	</ol>
+			<div>
+				<ol>
+					{
+						cart && cart.map( item => {
+							return (
+								<li key = {item.id}>
+									<h3>{item.title}</h3>
+									<p>{item.description}</p>
+									<p>Price: ${item.price}</p>
+								</li>
+							);
+						})
+					}
+				</ol>
+				<h3> Total: {total}</h3>
 
 	        </div>
 	        );
