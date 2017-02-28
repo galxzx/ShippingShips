161f4fe8947266e34e68e52c9015d661239c174b
Alan Campbell <alancampbell4444@gmail.com>
2017-02-28 08:58:46 -0800

    product api updated

--- server/products.js
+++ server/products.js
@@ -1,33 +1,26 @@
 'use strict'
 
-const express = require('express');
-const router = new express.Router();
 const db = require('APP/db')
 const Product = require('../db/models/product')
-module.exports = router;
+const Category = require('../db/models/category')
 
-
-router.get('/', function (req, res, next) {
-  Product.findAll()
-  .then(products => res.json(products))
-  .catch(next);
-});
-
-router.param('productId', function (req, res, next, theProductId) {
-  Product.findById(theProductId)
-  .then(product => {
-    if (!product) {
-      const err = Error('Product Not Found');
-      err.status = 404;
-      throw err
-    }
-    req.product = product;
-    next();
-    return null; 
-  })
-  .catch(next);
-});
-
-router.get('/:productId', function (req, res) {
-  res.json(req.product);
-});
+module.exports = require('express').Router()
+  .get('/', (req, res, next) =>
+    Product.findAll({ include:  [Category] })
+    .then(products => res.json(products))
+    .catch(next))
+  .param('productId', (req, res, next, theProductId) =>
+    Product.findOne({where: {id:theProductId}, include: [Category]})
+    .then(product => {
+      if (!product) {
+        const err = Error('Product Not Found');
+        err.status = 404;
+        throw err
+      }
+        req.product = product;
+        next();
+        return null; 
+    })
+    .catch(next))
+  .get('/:productId', (req, res) => 
+    res.json(req.product))
