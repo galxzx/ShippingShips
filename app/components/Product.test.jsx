40336fc13ee45e1cb34b277dc817829e6367af55
Corryn Young <young.corryn@gmail.com>
2017-03-06 11:54:11 -0500

    started product test

---
+++ app/components/Product.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import chai, {expect} from 'chai'
+chai.use(require('chai-enzyme')())
+import {shallow} from 'enzyme'
+import {spy} from 'sinon'
+chai.use(require('sinon-chai'))
+import {createStore} from 'redux'
+
+import ProductContainer from '../containers/ProductContainer'
+import Product from './Product'
